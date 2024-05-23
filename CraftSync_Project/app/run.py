2292cb59a2626b299c0e51ba24e36b265f177c20
Yamabooks <ok210200@oks.iput.ac.jp>
2024-05-23 17:52:17 +0900

    画像の表示は出来る

--- CraftSync_Project/app/run.py
+++ CraftSync_Project/app/run.py
@@ -7,7 +7,7 @@ app = Flask(__name__, static_folder = './templates/images2')
 
 @app.route('/')
 def index():
-    return render_template('test.html') #htmlファイルの表示
+    return render_template('page1.html') #htmlファイルの表示
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)

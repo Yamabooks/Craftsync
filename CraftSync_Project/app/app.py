882322aa6e1ad3a8904faf793df3e34212701fdd
Yamabooks <ok210200@oks.iput.ac.jp>
2024-05-27 18:27:29 +0900

    コメントを追加

--- CraftSync_Project/app/app.py
+++ CraftSync_Project/app/app.py
@@ -5,9 +5,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__, static_folder = './templates/images2')
 
+# [/]へアクセスがあった場合に、"page1.html"を返す
 @app.route('/')
 def index():
     return render_template('page1.html') #htmlファイルの表示
+
+# [/]
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)

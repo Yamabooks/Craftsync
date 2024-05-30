47fabc72a60f72d2e2159c8820ddc9434d85b307
Yamabooks <ok210200@oks.iput.ac.jp>
2024-05-30 14:51:35 +0900

    全ぺージ遷移を作成

--- CraftSync_Project/app/app.py
+++ CraftSync_Project/app/app.py
@@ -19,6 +19,50 @@ def introduction():
 def check():
     return render_template('page3.html')
 
+# [/]へアクセスがあった場合、"page4.html"を返す
+@app.route('/select')
+def select():
+    return render_template('page4.html')
+
+# [/]へアクセスがあった場合、"page5.html"を返す
+@app.route('/model')
+def model():
+    return render_template('page5.html')
+
+# [/]へアクセスがあった場合、"page6.html"を返す
+@app.route('/experience')
+def experience():
+    return render_template('page6.html')
+
+# [/]へアクセスがあった場合、"page7.html"を返す
+@app.route('/result1')
+def result1():
+    return render_template('page7.html')
+
+# [/]へアクセスがあった場合、"page8.html"を返す
+@app.route('/result2')
+def result2():
+    return render_template('page8.html')
+
+# [/]へアクセスがあった場合、"page9.html"を返す
+@app.route('/result3')
+def result3():
+    return render_template('page9.html')
+
+# [/]へアクセスがあった場合、"page10.html"を返す
+@app.route('/result4')
+def result4():
+    return render_template('page10.html')
+
+# [/]へアクセスがあった場合、"page11.html"を返す
+@app.route('/score')
+def score():
+    return render_template('page11.html')
+
+# [/]へアクセスがあった場合、"page12.html"を返す
+@app.route('/end')
+def end():
+    return render_template('page12.html')
 
 
 if __name__ == '__main__':

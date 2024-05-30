37a067e6239b86d70a3533e43ff7f86f8b39e700
Yamabooks <ok210200@oks.iput.ac.jp>
2024-05-30 13:38:50 +0900

    コミット

--- CraftSync_Project/app/app.py
+++ CraftSync_Project/app/app.py
@@ -1,9 +1,8 @@
-
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from flask import Flask, render_template
 
-app = Flask(__name__, static_folder = './templates/images2')
+app = Flask(__name__)
 
 # [/]へアクセスがあった場合に、"page1.html"を返す
 @app.route('/')
